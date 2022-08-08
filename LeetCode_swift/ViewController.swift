c267817906a0b3d2f04bd2d6bf2a960791b9c479
molierzhang <molierzhang@tencent.com>
2022-08-08 19:05:28 +0800

    2.两数相加3复习

--- LeetCode_swift/ViewController.swift
+++ LeetCode_swift/ViewController.swift
@@ -68,47 +68,76 @@ class ViewController: UIViewController {
 //        输出：[7,0,8]
 //        解释：342 + 465 = 807.
         
-        
-        /******学习（此题是基于以下原理）*****/
-        //        a 保存链表，用b去遍历 保存到a中
-        var a  = ListNode(1)
-        var b = a
-        
-//        同一个内存地址要加都加 所以b.next 就是a.next
-        b.next = ListNode(2)
-        
-//        把b指向 b的next 其实也就是a的next
-        b = b.next!
-        
-//        同一个内存地址下.next赋值都是同时赋值 所以a.next.next也一起被赋值
-        b.next = ListNode(3)
-        print(a,b)
-        /***************/
-        
-        var l11 = l1
-        var l22 = l2
-//        这里比较巧妙，直接吧进位值加到sum中一起计算，不用单独变量
-        var sum:Int = 0
-        let result:ListNode =  ListNode()
-        var current = result
-        while l11 != nil || l22 != nil {
-            if l11 != nil {
-                sum = sum + l11!.val
-                l11 = l11?.next
+        var list1 = l1
+        var list2 = l2
+        var sum = 0
+        let finalResult = ListNode(0)
+        var moveNode = finalResult
+        while list1 != nil || list2 != nil {
+            if list1 != nil {
+                sum += list1!.val
+                list1 = list1!.next
             }
-            if l22 != nil {
-                sum = sum + l22!.val
-                l22 = l22?.next
+            
+            if list2 != nil {
+                sum += list2!.val
+                list2 = list2!.next
             }
-            current.next = ListNode(sum % 10)
+            
+            moveNode.next = ListNode(sum % 10)
+            moveNode = moveNode.next!
             sum /= 10
-            current = current.next!
         }
         
-        //        最后要记得可能有进位
         if sum > 0 {
-            current.next = ListNode(sum % 10)
+            moveNode.next = ListNode(sum/10)
+            moveNode.next?.next = nil
         }
-        return result.next
+        
+        return finalResult.next
+        
+        
+        /*********************首次编码************************/
+//        /******学习（此题是基于以下原理）*****/
+//        //        a 保存链表，用b去遍历 保存到a中
+//        var a  = ListNode(1)
+//        var b = a
+//
+////        同一个内存地址要加都加 所以b.next 就是a.next
+//        b.next = ListNode(2)
+//
+////        把b指向 b的next 其实也就是a的next
+//        b = b.next!
+//
+////        同一个内存地址下.next赋值都是同时赋值 所以a.next.next也一起被赋值
+//        b.next = ListNode(3)
+//        print(a,b)
+//        /*****************************************/
+//
+//        var l11 = l1
+//        var l22 = l2
+////        这里比较巧妙，直接吧进位值加到sum中一起计算，不用单独变量
+//        var sum:Int = 0
+//        let result:ListNode =  ListNode()
+//        var current = result
+//        while l11 != nil || l22 != nil {
+//            if l11 != nil {
+//                sum = sum + l11!.val
+//                l11 = l11?.next
+//            }
+//            if l22 != nil {
+//                sum = sum + l22!.val
+//                l22 = l22?.next
+//            }
+//            current.next = ListNode(sum % 10)
+//            sum /= 10
+//            current = current.next!
+//        }
+//
+//        //        最后要记得可能有进位
+//        if sum > 0 {
+//            current.next = ListNode(sum % 10)
+//        }
+//        return result.next
     }
 }
